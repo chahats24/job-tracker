ce46e9e3e1273916e662961f5fc3db1b91c28cdd
Chahat Sahni <chahatsahni20@gmail.com>
2026-07-06 12:04:51 +0530

    feat: add User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,9 @@
-const { Schema, model } = require('mongoose');
+const {Schema, model} = require('mongoose');
 
 const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+  email : {type: String, required: true , unique: true},
+  password: {type: String, required:true}
+},{timestamps:true});
 
-module.exports = model('User', userSchema);
+module.exports=model('User',userSchema);
+
