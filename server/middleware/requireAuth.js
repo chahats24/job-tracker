7e15ab2e82b63b5426ea437f3d9b4524680f5197
Chahat Sahni <chahatsahni20@gmail.com>
2026-07-12 22:38:00 +0530

    feat: added Authorization middleware

---
+++ server/middleware/requireAuth.js
@@ -0,0 +1,17 @@
+const jwt=require('jsonwebtoken');
+const User=require('../models/User');
+const requireAuth = async (req,res,next)=> {
+    const {authorization}=req.headers;
+    if(!authorization){
+        return res.status(401).json({error:'Authorization token required'});
+    }
+    const token=authorization.split(' ')[1];
+    try{
+        const {id}=jwt.verify(token,'supersecretkey123');
+        req.user=await User.findOne({_id:id}).select('_id');
+        next();
+    }catch(err){
+        res.status(401).json({error:'Request not authorized'});
+    }
+};
+module.exports=requireAuth;
